a76c0d38204b4304e423c458befd22cd02653ed3
Ryan Strickler <ryan_s@knights.ucf.edu>
2020-08-31 23:52:40 -0400

    bolus bug fix

--- assets/javascript/bolus.js
+++ assets/javascript/bolus.js
@@ -124,8 +124,8 @@ function calculateBolus(bs, carbs, protein, active, activity) {
     bolusObj.active = active;
 
     // calc basal offset 
-    //           12am    1       2       3       4       5       6       7       8       9       10      11      12     1    2       3       4      5    6       7       8    9      10      11   12am   1       2 
-    var rates = [.9916, .9916, 1.0846, 1.1001, 1.1466, 1.1776, 1.224, 1.255, 1.2395, 1.1931, 1.1311, 1.0691, .9452, .8677, .8367, .8367, .8367, .8212, .8212, .8367, .8677, .8677, .9142, .9452, .9916, .9916, 1.0846];
+    //           12am    1       2       3       4       5       6       7       8       9       10      11      12     1    2       3       4      5    6       7       8    9      10      11   12am   1       2          3
+    var rates = [.9916, .9916, 1.0846, 1.1001, 1.1466, 1.1776, 1.224, 1.255, 1.2395, 1.1931, 1.1311, 1.0691, .9452, .8677, .8367, .8367, .8367, .8212, .8212, .8367, .8677, .8677, .9142, .9452, .9916, .9916, 1.0846, 1.1001];
     var time = new Date();
     var hour = time.getHours();
     if (time.getMinutes() > 30) {
@@ -134,7 +134,9 @@ function calculateBolus(bs, carbs, protein, active, activity) {
     console.log("current hour: " + hour);
     var offset = 0;
     for (let i = 0; i < 4; i++) {
+        console.log(hour);
         offset += rates[hour] - 1;
+        console.log(offset)
         hour++;
     }
     console.log("hourly offset: " + offset);
